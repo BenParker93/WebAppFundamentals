8b2bce5432c87fc6339a012b8fe29fee8339d430
BenParker93 <benparker_93@outlook.com>
2016-06-30 19:21:54 +0100

    middle of method

--- WebAppFundamentals/src/com/ben/DBUtils/DBConnection.java
+++ WebAppFundamentals/src/com/ben/DBUtils/DBConnection.java
@@ -4,6 +4,7 @@ public class DBConnection {
 	// test test test
 	// Hello this is the Desktop Work Commit now
 	public void firstMethod(){
-		
+		int letsSeeIfThisWorks = 69;
+		String hopeful = "Please god work";
 	}
 }
